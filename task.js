90c3e40683bdf366b200a34a2b26b499c549131c
a.pushkin <a.v.pushkin@gmail.com>
2023-06-06 17:37:55 +0300

    completed task_2

---
+++ task.js
@@ -0,0 +1,19 @@
+const btnAdd = document.getElementById('tasks__add')
+const tskList = document.getElementById('tasks__list')
+
+btnAdd.addEventListener('click', (event) => {
+    event.preventDefault()
+    const taskInput = document.getElementById('task__input').value
+    if (taskInput) {
+        const elementShell = document.createElement('div')
+        elementShell.classList.add('task')
+        elementShell.innerHTML = `<div class="task__title">${taskInput}</div><a href="#" class="task__remove">&times;</a>`
+        tskList.append(elementShell)
+        document.getElementById('task__input').value = ''
+        const btnRem = elementShell.querySelector('.task__remove')
+        btnRem.addEventListener('click', function(event) {
+            event.preventDefault()
+            btnRem.parentElement.remove()
+        })
+    }
+})
