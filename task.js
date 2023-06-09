34b6b7fe876027e311f421c05339e51cb0ae5bcc
a.pushkin <a.v.pushkin@gmail.com>
2023-06-09 11:16:35 +0300

    fix task_1

--- task.js
+++ task.js
@@ -4,20 +4,23 @@ hasTooltip.forEach(element => {
     tooltipElem = document.createElement('div')
     tooltipElem.innerHTML = element.title
     tooltipElem.classList.add('tooltip')
-    tooltipElem.setAttribute('data-position', 'top')
-    element.appendChild(tooltipElem)
-});
+    element.insertAdjacentElement('afterend', tooltipElem)
+})
 
 hasTooltip.forEach(element => {
     element.addEventListener('click', function(event) {
         event.preventDefault()
-        if (element.querySelector('.tooltip_active')) {
-            element.querySelector('.tooltip').classList.toggle('tooltip_active')
+        const tooltip = element.nextSibling;
+        domRect = element.getBoundingClientRect();
+        tooltip.style.left = `${domRect.x}px`
+
+        if (tooltip.classList.contains('tooltip_active')) {
+            tooltip.classList.toggle('tooltip_active')
         } else {
             document.querySelectorAll('.tooltip_active').forEach(el => {
                 el.classList.remove('tooltip_active')
             })
-            element.querySelector('.tooltip').classList.toggle('tooltip_active')
+            tooltip.classList.toggle('tooltip_active')
         }
     })
 })
