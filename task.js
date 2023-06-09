88f4cc24fdd186a79688eef4a2593476fcb61be5
a.pushkin <a.v.pushkin@gmail.com>
2023-06-09 12:13:34 +0300

    fix task_3

--- task.js
+++ task.js
@@ -19,25 +19,22 @@ products.forEach(el => {
 addBtns.forEach(el => {
     el.addEventListener('click', function(event) {
         event.preventDefault()
-        let imgPath = el.closest('.product').querySelector('.product__image').getAttribute('src')
         let productID = el.closest('.product').getAttribute('data-id')
-        let productBasket = document.createElement('div')
         let productQty = el.parentElement.querySelector('.product__quantity-value').textContent
-        let checker = true
+        let exstProduct = Array.from(document.querySelectorAll('.cart__product')).find(el => el.dataset.id === productID)
+        
+        if (parseInt(productQty)>0 && exstProduct){
+            let qurQty = exstProduct.querySelector('.cart__product-count').textContent
+            exstProduct.querySelector('.cart__product-count').textContent 
+            = parseInt(qurQty) + parseInt(productQty)
 
-        if (parseInt(productQty)>0){
-            const productsBasket = document.querySelectorAll('.cart__product')
-            productsBasket.forEach(el => {
-                if (el.getAttribute('data-id') == productID) {
-                    let qurQty = el.querySelector('.cart__product-count').textContent
-                    el.querySelector('.cart__product-count').textContent = parseInt(qurQty) + parseInt(productQty)
-                    checker = false
-                }})
-        if (checker) {
+        } else {
+            let imgPath = el.closest('.product').querySelector('.product__image').getAttribute('src')
+            let productBasket = document.createElement('div')
             basket.appendChild(productBasket)
             productBasket.classList.add('cart__product')
             productBasket.setAttribute('data-id', productID)
             productBasket.innerHTML = `<img class="cart__product-image" src=${imgPath}><div class="cart__product-count">${productQty}</div><div class="product__del">Удалить</div>`
             productBasket.querySelector('.product__del').addEventListener('click', () => {
                 productBasket.remove()
-            })}}})})
+            })}})})
